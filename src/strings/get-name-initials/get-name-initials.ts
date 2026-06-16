412d9fa07583a5a98c58d2379aac4a694b1d05e2
pvslvch <polina.vasilevichh@gmail.com>
2026-06-16 10:27:22 +0200

    feat(get-name-initials): add  task solution

--- src/strings/get-name-initials/get-name-initials.ts
+++ src/strings/get-name-initials/get-name-initials.ts
@@ -3,3 +3,10 @@ export function getNameInitials(name: string): string {
 
   return `${firstName[0].toUpperCase()}.${lastName[0].toUpperCase()}.`;
 }
+
+export function getNameInitialsV2(name: string): string {
+  return name
+    .split(" ")
+    .map((el) => `${el.slice(0, 1).toUpperCase()}.`)
+    .join("");
+}
