5e2944e85ee61f2121fb1cfa30b9a219695153a9
pvslvch <polina.vasilevichh@gmail.com>
2026-06-18 11:15:46 +0200

    feat(is-palindrome): add  task solution

--- src/strings/is-palindrome/solution.ts
+++ src/strings/is-palindrome/solution.ts
@@ -2,3 +2,15 @@ function isPalindrome(str: string): boolean {
   const normalized = str.toLowerCase();
   return normalized === normalized.split("").reverse().join("");
 }
+
+function isPalindromeV2(str: string): boolean {
+  const cleanedStr = str.replace(/[^a-zA-Z0-9]/g, "").toLowerCase();
+
+  for (let i = 0; i < Math.floor(cleanedStr.length / 2); i++) {
+    if (cleanedStr[i] !== cleanedStr[cleanedStr.length - 1 - i]) {
+      return false;
+    }
+  }
+
+  return true;
+}
